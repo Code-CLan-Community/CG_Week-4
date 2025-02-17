ad6c08b9acadbf159e580dbaa16a201128711771
Code-CLan-Community <veergupta0801@gmail.com>
2025-02-18 00:55:36 +0530

    [Rishabh] Add. Database Connections Basic JUnit Problem Testing

---
+++ java_JUnit_Testing/src/test/java/basicjunittesting/databaseconnectiontest/DatabaseConnectionTest.java
@@ -0,0 +1,33 @@
+package basicjunittesting.calculatortest.databaseconnectiontest;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import basicJunitproblems.calculator.databaseconnection.DatabaseConnection;
+
+public class DatabaseConnectionTest {
+    private DatabaseConnection dbConnection;
+
+    @BeforeEach
+    public void setUp() {
+        dbConnection = new DatabaseConnection();
+        dbConnection.connect();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        dbConnection.disconnect();
+    }
+
+    @Test
+    public void testConnectionEstablished() {
+        assertTrue(dbConnection.isConnected(), "Database should be connected");
+    }
+
+    @Test
+    public void testConnectionClosed() {
+        dbConnection.disconnect();
+        assertFalse(dbConnection.isConnected(), "Database should be disconnected");
+    }
+}

c3384207d2732b11e4a45de15e99504a3c43a630
Code-CLan-Community <veergupta0801@gmail.com>
2025-02-18 00:48:01 +0530

    [Rishabh] Add. StringUtils  Main Class Basic JUnit Problem

---
+++ java_JUnit_Testing/src/main/java/basicJunitproblems/stringutility/StringUtils.java
@@ -0,0 +1,16 @@
+package basicJunitproblems.calculator.stringutility;
+
+public class StringUtils {
+    public String reverse(String str) {
+        return new StringBuilder(str).reverse().toString();
+    }
+
+    public boolean isPalindrome(String str) {
+        String reversed = reverse(str);
+        return str.equalsIgnoreCase(reversed);
+    }
+
+    public String toUpperCase(String str) {
+        return str.toUpperCase();
+    }
+}

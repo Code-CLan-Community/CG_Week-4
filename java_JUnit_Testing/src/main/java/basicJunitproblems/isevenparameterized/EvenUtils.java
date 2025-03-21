77299568e5f9c84971cc8d425b0b1e3f586a31b5
Rishabh Gupta <168972221+Code-CLan-Community@users.noreply.github.com>
2025-03-21 12:37:53 +0530

    Delete java_JUnit_Testing directory

--- java_JUnit_Testing/src/main/java/basicJunitproblems/isevenparameterized/EvenUtils.java
+++
@@ -1,7 +0,0 @@
-package basicJunitproblems.calculator.isevenparameterized;
-
-public class EvenUtils {
-    public boolean isEven(int number) {
-        return number % 2 == 0;
-    }
-}
--- java_JUnit_Testing/src/main/java/basicJunitproblems/performance/Performance.java
+++
@@ -1,8 +0,0 @@
-package basicJunitproblems.calculator.performance;
-
-public class Performance {
-    public String longRunningTask() throws InterruptedException {
-        Thread.sleep(1500); //(3 seconds)
-        return "Task Completed";
-    }
-}
